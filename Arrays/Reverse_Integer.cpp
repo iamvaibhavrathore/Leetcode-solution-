65f856fc5a49b82d5c15b0bc2279f4273c782483
Vaibhav Rathore <94102924+iamvaibhavrathore@users.noreply.github.com>
2022-12-21 00:00:39 +0530

    Create Reverse_Integer.cpp
    Solution for reverse integer

---
+++ Arrays/Reverse_Integer.cpp
@@ -0,0 +1,45 @@
+/*
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+
+
+*/
+
+/* CODE */
+
+class Solution {
+public:
+    int reverse(int x) {
+
+        long long int ans=0;
+        if(x/10==0){
+            return x;
+        }
+        while(x){
+            ans = ans*10 + x%10;
+            x=x/10;
+        }
+        return (ans<INT_MIN || ans>INT_MAX)?0:ans;
+    }
+};
